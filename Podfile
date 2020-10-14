72b971af0123c95ddc042c71408dabe18a259ff2
tubone24 <tubo.yyyuuu@gmail.com>
2020-10-15 00:24:31 +0900

    wip

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'SpotifyMenuBar' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
   pod 'APIKit', '~> 5.0'
+  pod "SpotifyKit"
 
   # Pods for SpotifyMenuBar
 
--- SpotifyMenuBar/AppDelegate.swift
+++ SpotifyMenuBar/AppDelegate.swift
@@ -7,20 +7,40 @@
 //
 
 import Cocoa
+import SpotifyKit
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    @IBAction func authorize(_ sender: Any) {
+        spotifyManager.authorize()
+    }
+    @IBAction func deauthorize(_ sender: Any) {
+        spotifyManager.deauthorize()
+    }
+    
     //Outlet
     @IBOutlet weak var menu: NSMenu!
+    
+    let spotifyManager = SpotifyManager(with:
+        SpotifyManager.SpotifyDeveloperApplication(
+            clientId:     "d42df9084ede4a6f95131a31a641cbd8",
+            clientSecret: "0090a1db6b7f46a2938792bef6bcbcce",
+            redirectUri:  "spotifymenubar://callback"
+        )
+    )
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-
-
+        
+        self.initEventManager()
+        
+        
         // icon image
         self.statusItem.button?.image = NSImage(imageLiteralResourceName: "menuBarIcon")
         
@@ -52,7 +72,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         previousItem.title = "Previous"
         
-        previousItem.action = #selector(AppDelegate.quit(_:))
+        previousItem.action = #selector(authorize)
         
         menu.addItem(previousItem)
 
@@ -64,11 +84,35 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         quitItem.action = #selector(AppDelegate.quit(_:))
         
         menu.addItem(quitItem)
+        
+        self.spotifyManager.deauthorize()
+        self.spotifyManager.authorize()
 
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+    
+    func initEventManager() {
+        NSAppleEventManager.shared().setEventHandler(self,
+                                                     andSelector: #selector(handleURLEvent),
+                                                     forEventClass: AEEventClass(kInternetEventClass),
+                                                     andEventID: AEEventID(kAEGetURL))
+    }
+    //うまく行かない
+    @objc func handleURLEvent(event: NSAppleEventDescriptor,
+                              replyEvent: NSAppleEventDescriptor) {
+        if  let descriptor = event.paramDescriptor(forKeyword: keyDirectObject),
+            let urlString  = descriptor.stringValue,
+            let urlComponents = URLComponents(string: urlString),
+            let queryItems    = (urlComponents.queryItems as [NSURLQueryItem]?) {
+            let code = queryItems.filter({ (item) in item.name == "code" }).first?.value!
+            if let authorizationCode = code {
+                NSLog(code!)
+                spotifyManager.saveToken(from: authorizationCode)
+            }
+        }
     }
 
     @objc func quit(_ sender: Any){
@@ -76,5 +120,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         NSApplication.shared.terminate(self)
     }
 }
+
 
 
